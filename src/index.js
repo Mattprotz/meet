d6de5565399af29f261a4377a40c79122e661a62
Matthew Proteau <matthew.proteau@gmail.com>
2024-03-18 15:08:54 -0700

    setting up atatus

--- src/index.js
+++ src/index.js
@@ -25,4 +25,3 @@ serviceWorkerRegistration.unregister();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-atatus.notify(new Error('Test Atatus Setup'));
