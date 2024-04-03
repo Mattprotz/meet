36c1d40068df35af89d83720c4843a25833963f5
Matthew Proteau <matthew.proteau@gmail.com>
2024-04-03 15:30:45 -0700

    update

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -41,4 +41,42 @@ class InfoAlert extends Alert{
     }
 }
 
-export default Alert;
+class WarningAlert extends Component{
+    constructor(props){
+        super(props);
+        this.color = 'orange'
+    }
+    getStyle = () =>{
+        return{
+            color: this.color,
+        }
+    }
+    render(){
+        return(
+            <div className="Alert">
+                <p style={this.getStyle()}>{this.props.text}</p>
+            </div>
+        )
+    }
+}
+
+class ErrorAlert extends Component{
+    constructor(props){
+        super(props);
+        this.color = 'red'
+    }
+    getStyle = () =>{
+        return{
+            color: this.color,
+        }
+    }
+    render(){
+        return(
+            <div className="Alert">
+                <p style={this.getStyle()}>{this.props.text}</p>
+            </div>
+        )
+    }
+}
+
+export default {Alert, InfoAlert, ErrorAlert, WarningAlert} ;
